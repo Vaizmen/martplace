37fab0b6efbbfc2ecbf0f3189dd7c25f6a39a308
Vaizmen <awfawdfwa@gmail.com>
2020-04-12 19:38:53 +0300

    added all section

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,11 @@ $(function(){
     $(".products__inner").slick({
 
     });
+
+    $('.feed__slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
     var mixer = mixitup('.item__product');
     
 });
