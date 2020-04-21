58986d4e2f710dd36fb825c5399c3ccbeeac4c92
Vaizmen <awfawdfwa@gmail.com>
2020-04-21 14:49:38 +0300

    added new items and button list

--- app/js/main.js
+++ app/js/main.js
@@ -8,6 +8,30 @@ $(function(){
         slidesToShow: 3,
         slidesToScroll: 3
     });
+    $(".products-star").rateYo({
+        starWidth: "15px",
+        rating: 4.5,
+        readOnly: true
+      });
+      $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 350,
+        from: 30,
+        to: 300,
+        prefix: "$"
+    });
+    $(".filter-bar__listsvg").on('click', function(){
+        $('.items__inner-box').addClass('list');
+        $('.filter-bar__listsvg').addClass('active');
+        $('.filter-bar__greed').removeClass('active');
+    });
+    
+    $(".filter-bar__greed").on('click', function(){
+        $('.items__inner-box').removeClass('list');
+        $('.filter-bar__greed').addClass('active');
+        $('.filter-bar__listsvg').removeClass('active');
+    });
     var mixer = mixitup('.item__product');
     
 });
